18ef51e2e172c57ca1bd7cc0fe31ed18feecd318
NilsonKr <nuotrok444@gmail.com>
2021-01-16 05:42:52 -0500

    Building the esential structure

---
+++ src/templates/header.js
@@ -0,0 +1,14 @@
+const header = () => {
+    const view = `
+        <div class="home">
+            <span>icon</span>
+            <h2>Home</h2>
+        </div>
+        <div class="details">
+            <h3>Contact</h3>
+            <h3>About</h3>
+        </div>
+    `
+   return view
+}
+
