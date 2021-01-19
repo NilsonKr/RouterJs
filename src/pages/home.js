d6b2edfa6192d8f15d054ea70e2877e6293dbc8b
NilsonKr <nuotrok444@gmail.com>
2021-01-19 03:13:10 -0500

    Compiled the code with webpack and adding the principal styles to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,18 +10,21 @@ const home = () => {
             <a href="/start_post">
                 <img src="https://my1.fr/blog/wp-content/uploads/2019/06/developer-guide-blog-2.png" alt="begin image">
                 <h2>Which Path shall i pick?</h2>
+                <button>See...</button>
             </a>
         </div>
         <div class="posts">
             <a href="/game_post">
                 <img src="https://miro.medium.com/max/10368/0*OlVTuxFz-Qn7oTUn" alt="Game Development image">
                 <h2>Talk a few about Game's Development...</h2>
+                <button>See...</button>
             </a>
         </div>
         <div class="posts">
             <a href="spa_post">
                 <img src="https://files.virgool.io/upload/users/92509/posts/lj9q4pxscrj2/v31nyjsrnkrl.jpeg" alt="SPA image">
                 <h2>What a H***k is a SPA?</h2>
+                <button>See...</button>
             </a>
         </div>
     </section>
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+const { resolve } = require('path')
+
+module.exports = {
+    entry:'./src/main.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename : 'main.js'
+    },
+    resolve:{
+        extensions : ['.js']
+    },
+    module:{
+        rules:[
+            {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader:'babel-loader'
+            },
+        }]
+    },
+    plugins :[
+        new htmlWebpackPlugin({
+            inject: true,
+            template: './index.html',
+            filename: './index.html',
+        }),
+        new copyWebpackPlugin({
+            patterns: [{
+                from:'./styles/style.css',
+                to:''
+            }]
+        })
+    ]
+}
