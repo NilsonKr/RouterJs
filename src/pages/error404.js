6a70164b16027995f6255344f2db82f045eb51fc
NilsonKr <nuotrok444@gmail.com>
2021-01-31 03:25:07 -0500

    Typo error fixed

--- src/pages/error404.js
+++ src/pages/error404.js
@@ -1,7 +1,7 @@
 const error404 = () => {
     const view = `
     <div class="error404">
-        <h2>ERRO404 Not Found :( </h2>
+        <h2>ERROR404 Not Found :( </h2>
         <span></span>
     </div>
     `
